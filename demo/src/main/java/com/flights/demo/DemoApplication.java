e006d106a54a59dabbb7bc1a423b018e03a568f3
dancasnar1 <dancasnar1@alum.us.es>
2022-11-29 12:06:00 +0100

    Created Beans
    Create beans for microservices

--- demo/src/main/java/com/flights/demo/DemoApplication.java
+++ demo/src/main/java/com/flights/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.flights.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.demo")
+@EnableMongoRepositories("com.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
